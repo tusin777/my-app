fd11a8b37b730dded315fb7f8ff856f6f0e44996
tusin777 <ruslantusin@gmail.com>
2026-01-02 00:33:37 +0100

    Урок 1.6_старт

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,11 @@
-import Email from "../Email";
-import MyName from "../MyName/MyName";
 import "./App.css";
 
 function App() {
-  const name = `Вася Пупкин`;
 
-  const element = <h1>Алексей и {name} - друзья</h1>;
-
-  const condition = true;
-
-  const response = "<div>alert('Вы взломаны!')</div>";
 
   return (
     <>
-      <h1>Привет, React!</h1>
-      <p>Это мой первый React-проект с Vite</p>
-      {element}
-      <div dangerouslySetInnerHTML={{ __html: response }}></div>
-      <span>{3 + 8}</span>
-      {condition && <MyName />}
-      <Email />
-      <br />
-      <input type="checkbox" checked={false} />
-      <img src="" alt="" />
-      <br />
-      <label htmlFor="email"></label>
-      <button disabled>Просто кнопка</button>
+      
     </>
   );
 }
