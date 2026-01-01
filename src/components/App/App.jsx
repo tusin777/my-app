3c6015536d7dd492a496feac49e027d238334410
tusin777 <ruslantusin@gmail.com>
2026-01-01 18:33:45 +0100

    lesson-1.4: project structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import MyName from "../MyName/MyName";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Привет, React</h1>
+      <p>Это мой первый компонент на React. Проект запущен через Vite.</p>
+      <p>Изучаю компоненты, JSX и структуру проекта.</p>
+      <MyName />
+      <MyName />
+      <MyName />
+    </>
+  );
+}
+
+export default App;
