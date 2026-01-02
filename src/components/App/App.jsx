136504bdaa30ced2dee6db413e78d6d11db0153a
tusin777 <ruslantusin@gmail.com>
2026-01-02 20:41:21 +0100

    Урок 1.11

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,20 @@
 import Header from "../Header/Header";
 import HomePage from "../../pages/HomePage";
-import { ThemeToggle } from "../ThemeToggle";
 import { Footer } from "../Footer/Footer";
 import "./App.css";
-import { useState } from "react";
+import Counter from "../Counter";
+import UserProfile from "../UserProfile";
+import ParentComponent from "../ParentComponent";
 
 function App() {
-  const [clicks, setClicks] = useState(5);
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [text, setText] = useState("Какой-то текст");
   return (
     <main className="app">
-      <Header clicks={clicks} text={text} />
-      <HomePage clicks={clicks} setClicks={setClicks} />
-      <ThemeToggle isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-      <Footer isDarkMode={isDarkMode} setText={setText} text={text} />
+      <Header />
+      <HomePage />
+      <Counter />
+      <UserProfile />
+      <ParentComponent />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/ChildComponent.jsx
@@ -0,0 +1,24 @@
+// import { Component } from "react";
+
+// export class ChildComponent extends Component {
+//   render() {
+//     const { count, onIncrement } = this.props;
+//     return (
+//       <div>
+//         <p>Еще счетчик: {count}</p>
+//         <button onClick={onIncrement}>Прибавить 1</button>
+//       </div>
+//     );
+//   }
+// }
+
+const ChildComponent = (props) => {
+  return (
+    <div>
+      <p>Еще счетчик: {props.count}</p>
+      <button onClick={props.onIncrement}>Прибавить 1</button>
+    </div>
+  );
+};
+
+export default ChildComponent;
